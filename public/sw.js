1d4364740e70a63cb9b8d8b222e83a98d1aaefb1
Tomo Masakura <tomo.masakura@gmail.com>
2016-06-11 01:24:42 +0900

    fix bug

--- public/sw.js
+++ public/sw.js
@@ -2,4 +2,12 @@
 
 (function () {
     console.log('start sw worker');
+
+    self.addEventListener('push', event => {
+        const notification = self.registration.showNotification('PWA Notification', {
+            body: 'registration',
+            icon: '/app-icon.png'
+        });
+        event.waitUntil(notification);
+    });
 })();
