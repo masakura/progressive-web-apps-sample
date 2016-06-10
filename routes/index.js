a4a6892fc12903ab4d8c8b7fc0d50799f962079c
Tomo Masakura <tomo.masakura@gmail.com>
2016-06-10 23:59:06 +0900

    About を追加

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'PWA Sample' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about', {});
 });
 
 module.exports = router;
